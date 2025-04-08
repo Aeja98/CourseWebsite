ec3ee6260c9f9b7ca77546f99cdf41d291ea4b57
Angelica Adolfsson <angelicaadolfsson1@gmail.com>
2025-04-08 17:14:35 +0200

    Final changes
    Finished all the media queries
    Added text/info to about page
    Installed dotenv & created env file with my database info & example env
    Updated server.js to read env info
    Created readme with clear instructions & code snippits & folder structure
    Upload ER-Diagram
    Updated package.json -> removed unnecessary dependencies & scripts
    Updated gitignore
    
    Reviewed assignments & double checked to make sure everything is included

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 
+//Import framework
 const express = require("express");
 const mysql = require("mysql2");
 const cors = require("cors");
@@ -9,12 +10,15 @@ const port = 3000;
 app.use(cors());
 app.use(express.json());
 
-//Access my database
+//Hämta variabler
+require("dotenv").config();
+
+//Access database
 const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "Juno2024!",
-  database: "courses"
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 //connect to database - or return error mess
