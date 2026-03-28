5de16344b14ca33bf9e455576b352b85425ebe13
Angelica Adolfsson <angelicaadolfsson1@gmail.com>
2026-03-28 13:58:52 +0100

    Add files via upload

--- server.js
+++ server.js
@@ -19,10 +19,13 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false,
-  }
 });
+
+if (process.env.NODE_ENV === "production") {
+  dbConfig.ssl = {
+    rejectUnauthorized: false
+  };
+}
 
 //connect to database - or return error mess
 db.connect((err) => {
