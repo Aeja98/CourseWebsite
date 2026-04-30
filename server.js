0bbc61d16d97942333ae2f3559ec572bff53c495
Angelica Adolfsson <angelicaadolfsson1gmail.com>
2026-04-30 15:40:54 +0200

    updated databse info

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.set("views", path.join(__dirname, "views"));
 //Database connection
 const db = mysql.createConnection({
   host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
